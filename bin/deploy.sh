7ca88b9af6155f65f5b6017dca41d77e78fde1aa
Ni Yue <niyue.com@gmail.com>
2014-10-08 14:46:04 +0800

    Update deploy script to start/stop server.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
+# set environment varibles in secrets
 echo "Running deployment script..."
+source ~/.bash_profile
 
 echo "Redirecting port 80 to 9292..."
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 9292
 sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 9292
+
+bundle install
+
+echo "Performing backup..."
+RAILS_ENV=production backup perform --trigger zhi_backup -c db/backup.rb
+
+echo "Migrate database..."
+RAILS_ENV=production rake db:migrate
+
+echo "Stopping server..."
+bundle exec pumactl --pidfile log/puma.pid stop
+
+echo "Starting server..."
+bundle exec puma -e production -d --pidfile log/puma.pid
 
 echo "Server is started."
