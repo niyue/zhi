1a454e64803a823d207d8498d2d530f5783c7705
Ni Yue <niyue.com@gmail.com>
2014-10-08 15:07:58 +0800

    Enable static assets serving in production mode.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,6 +9,7 @@ sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 9292
 sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 9292
 # sudo yum -y install sqlite-devel
 
+gem install bundle
 bundle install
 
 echo "Performing backup..."
