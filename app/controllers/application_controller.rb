d4bdb01384d675e9a3445d99b4eb1a643f25c52e
Ni Yue <niyue.com@gmail.com>
2014-10-08 10:57:43 +0800

    Add HTTP basic authentication for the application.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: Rails.application.secrets.http_basic_auth_user, 
+    password: Rails.application.secrets.http_basic_auth_password unless Rails.env.test?
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
