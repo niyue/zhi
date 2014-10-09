0339a6d2c62f123409a077c9efc0ede4d1a247dc
Ni Yue <niyue.com@gmail.com>
2014-10-09 18:06:10 +0800

    Update http basic authentication realm.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
+require 'socket'
+
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with name: Rails.application.secrets.http_basic_auth_user, 
-    password: Rails.application.secrets.http_basic_auth_password unless Rails.env.test?
+    password: Rails.application.secrets.http_basic_auth_password, 
+    realm: Socket.gethostname unless Rails.env.test?
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
