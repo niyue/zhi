310e551de6514676b68fbd45e2bffa5b1ce42288
Ni Yue <niyue.com@gmail.com>
2014-10-10 13:50:29 +0800

    Replace relative url with absolute url for ckeditor image assets so that wkhtmltopdf can find them.

--- app/controllers/printable_exams_controller.rb
+++ app/controllers/printable_exams_controller.rb
@@ -2,14 +2,18 @@ class PrintableExamsController < ApplicationController
   before_action :set_exam, only: [:show]
   
   def show
-    @multiple_choices = @exam.multiple_choices;
-    @essays = @exam.essays;
+    
+    @multiple_choices = @exam.multiple_choices
+    @essays = @exam.essays
     template = params[:answer] == 'true' ? 'show_with_answer' : 'show'
     respond_to do |format|
       format.html do
         render template, layout: false
       end
       format.pdf do
+        
+        apply_absolute_image_url
+        
         @custom_cover_file = File.join(Rails.root, 'public', 'files', 'cover.pdf.erb')
         @use_custom_cover_file = File.exists?(@custom_cover_file)
         
@@ -24,6 +28,17 @@ class PrintableExamsController < ApplicationController
   private
   def set_exam
     @exam = Exam.find(params[:id])
+  end
+  
+  def apply_absolute_image_url
+    ckeditor_path = Rails.root.join('public', 'ckeditor_assets').to_s
+    @multiple_choices.each do |mc|
+      mc.description.gsub!(/\/ckeditor_assets/, ckeditor_path)
+    end
+    
+    @essays.each do |e|
+      e.description.gsub!(/\/ckeditor_assets/, ckeditor_path)
+    end
   end
   
 end
